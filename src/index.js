ae9fd6018c01d9c00085d17d062bdd2fe6b515e8
Abdul-Gafur <abdul.taramov2001@gmail.com>
2021-02-26 15:26:41 +0300

    overloaded version

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import Game from './App';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import './App.css'
+import Game from './App.js'
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Game />
-  </React.StrictMode>,
+  <Game />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
